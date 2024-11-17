9a73c39038163307e192f1a128b659d5417af915
NithinKS007 <nithinksndkm@gmail.com>
2024-11-17 21:38:44 +0530

    Implement localStorage to save todo data

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,34 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 
 const Todo = () => {
-
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
 
-  const handleInputChange  = (e) => {
+  const handleInputChange = (e) => {
     setTask(e.target.value);
   };
 
-  const handleAddTodo  = (e) => {
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    console.log(storedTodos);
+    
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos]);
+
+  const handleAddTodo = (e) => {
     e.preventDefault();
 
     if (task.trim()) {
-        const newTodo  = { id: Date.now(), task:task, isCompleted:false, isEditing:false }
-        setTodos([...todos,newTodo ]); 
-        setTask(""); 
-      }
-  }; 
+      const newTodo = {
+        id: Date.now(),
+        task: task,
+        isCompleted: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTask("");
+    }
+  };
 
   const toggleComplete = (id) => {
-
-      setTodos(todos.map(item => item.id === id ? {...item,isCompleted:!item.isCompleted}:item))
-  }
+    setTodos(
+      todos.map((item) =>
+        item.id === id ? { ...item, isCompleted: !item.isCompleted } : item
+      )
+    );
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
       <div className="bg-white p-6 rounded-xl shadow-md w-1/2">
-        <h1 className="text-xl font-semibold text-center text-gray-700 mb-4">Todo List</h1>
+        <h1 className="text-xl font-semibold text-center text-gray-700 mb-4">
+          Todo List
+        </h1>
         <form className="flex gap-2 mb-4" onSubmit={handleAddTodo}>
           <input
             type="text"
@@ -44,10 +67,10 @@ const Todo = () => {
             Add
           </button>
         </form>
-        <TodoList todos={todos} toggleComplete ={toggleComplete} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} />
       </div>
     </div>
   );
-}
+};
 
 export default Todo;
